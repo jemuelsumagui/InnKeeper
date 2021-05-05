cdd67f6e3c042cc93f6c230bde2fe57c2755c635
jemuelsumagui <51130636+jemuelsumagui@users.noreply.github.com>
2021-05-05 21:31:08 +0200

    Delete esempio directory

--- esempio/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('TITOLO',	'Gestione strutture');            //definisci queste variabili con: titolo, nome dell'host
-	define('DBMS_HOST',	'127.0.0.1');                     //nome dell'utente, password e nome del database
-	define('DBMS_USER',	'root');
-	define('DBMS_PASSWORD',	'');
-	define('DBMS_DB',	'test');
-?>
--- esempio/tpl/head.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include_once("./head_meta.tpl.php");
-    include_once("./head_css.tpl.php");
-    include_once("./head_js.tpl.php");
-?>
